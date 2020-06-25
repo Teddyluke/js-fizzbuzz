568d790aae6cd2aa468538f82878ba99a8ae9352
Luca Maglie <luca.maglie2@gmail.com>
2020-06-25 14:50:49 +0200

    inseriti elementi nell'html in maniera grezza.

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
 // Per i multipli di 5 stampi "Buzz" al posto del numero.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero.
 
+//  creo arrey vuoto dove inserire tutti i valori
 var numeri = [];
-
+// creo un ciclo for che scorra fino a 100 inserendo i valori nell'arrey come definiti nell if
 for (var i = 1; i < 101; i++) {
   if ((i % 3 == 0) && (i % 5 == 0)) {
     numeri.push("FizzBuzz")
@@ -20,3 +21,10 @@ for (var i = 1; i < 101; i++) {
 }
 
 console.log(numeri)
+
+//  stampo nell html la lista degli elementi.
+var listaNumeri = document.getElementById('listaNumeri');
+
+for (var i = 0; i < numeri.length; i++) {
+  listaNumeri.innerHTML += "<li>" + numeri[i] + "</li>";
+}
